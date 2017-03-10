810c9b19aa77e75e1bc616f7d32d4b460bdadd65
skbabu <babujob@gmail.com>
2017-03-10 10:54:13 +0200

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+
+[assembly: AssemblyTitle("WeatherApp")]
+[assembly: AssemblyProduct("WeatherApp")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
